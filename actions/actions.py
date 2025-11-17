3aa5b7f8351415b5407902dd62f6e132b5974155
Caroline Dolan Gilman <cdd51@duke.edu>
2025-11-17 12:45:00 -0500

    language check attempt

--- actions/actions.py
+++ actions/actions.py
@@ -1,8 +1,29 @@
 from typing import Any, Dict, List, Text
+from langdetect import detect
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
+
+class ActionSessionStart(Action):
+    def name(self) -> Text:
+        return "action_session_start"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        # hard-coded balance for tutorial purposes. in production this
+        # would be retrieved from a database or an API
+
+        answer = tracker.latest_message.text
+        print(answer)
+        lang = langdetect.detect(answer)
+
+        return [SlotSet("language", lang)]
+
 
 
 class ActionCheckSufficientFunds(Action):
@@ -21,3 +42,6 @@ class ActionCheckSufficientFunds(Action):
         transfer_amount = tracker.get_slot("amount")
         has_sufficient_funds = transfer_amount <= balance
         return [SlotSet("has_sufficient_funds", has_sufficient_funds)]
+
+
+
